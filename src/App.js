f01bd18329e1b279c1b3989ed9f0c7c4fff95d60
Oksana <okkaragicheva@gmail.com>
2019-01-15 09:50:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+      this.state = {
+        posts: [],
+        comments: [],
+        newComment: "",
+        showAllPosts: true,
+        showPostID: 0
+      };
+  }
+
+componentDidMount() {
+  let url = "http://localhost:3001/posts";
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+         this.setState({
+           posts: data
+         });
+      });
+}
+
+showPost = (id) => {
+  this.setState({
+    showPostID: id,
+    showAllPosts: false
+  });
+    let url1 = "http://localhost:3001/comments";
+      fetch(url1)
+       .then(resp => resp.json())
+       .then(data => {
+          this.setState({
+            comments: data
+          });
+       });
+}
+
+inputComment = (e) => {
+   this.setState({
+     newComment: e.target.value
+   });
+}
+
+addNewComment = (comment) => {
+  let commentsUrl = "http://localhost:3001/comments";
+    let options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(comment)
+    }
+      return fetch(commentsUrl, options)
+        .then((response) => response.json)
+        .then(data => {
+           this.setState({
+             comments: [...this.state.comments, comment],
+             newComment: ""
+           });
+        });
+}
+
+closePost = () => {
+  this.setState({
+    showAllPosts: true
+  });
+}
+
   render() {
+
+    const currentComments = this.state.comments.filter((comment, index) => {
+      return comment.postId === this.state.showPostID;
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {
+          this.state.showAllPosts ? this.state.posts.map((post, index) => {
+          return (
+            <div key={index}>
+              <h3 className="post-title" onClick={() => this.showPost(post.id)}>{post.title}</h3>
+            </div>
+          )
+        })
+        :
+        this.state.posts.map((post, index) => {
+          return (
+           <div key={index}>
+             {
+               post.id === this.state.showPostID ? <div><h3 className="post-title" onClick={this.closePost}>{post.title}</h3>
+               <p className="post-body">{post.body}</p>
+               {
+                 currentComments.map((comment, index) => {return <div key={index} className="comments"><p>{comment.body}{comment.time}</p></div>})
+               }
+               </div>
+               :
+               null
+             }
+           </div>
+          )
+        })
+        }
+          {
+            !this.state.showAllPosts ? <div><input type="text" placeholder="Input the comment..." value={this.state.newComment} onChange={this.inputComment}/>
+            <button type="submit" value="Submit" onClick={() => this.addNewComment({"body": this.state.newComment, "postId": this.state.showPostID})}>Add Comment</button></div> : null
+          }
       </div>
     );
   }
